5db60e096da31f1b528197654139779d44c6280a
johnlin <john830110@gmail.com>
2024-02-10 00:33:24 +0800

    feat: 添加 Vuetify 設定

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,12 @@
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
