b93b0e8f1340b4abd1c806e1cf953653ddbec5a6
johnlin <john830110@gmail.com>
2024-02-10 18:38:49 +0800

    feat: 新增判斷點擊 v-card 左或右側

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 
+const detectClickSide = (event: MouseEvent) => {
+  const rect = (event.target as HTMLElement).getBoundingClientRect();
+  const middlePoint = rect.left + rect.width / 2;
+
+  if (event.clientX < middlePoint) {
+    console.log('Left side clicked');
+  } else {
+    console.log('Right side clicked');
+  }
+};
+
 </script>
 
 <template>
-  <v-card>
+  <v-card  @click="detectClickSide($event)">
     <v-card-title>Home</v-card-title>
     <v-card-text>
       <p>Welcome to the home page</p>
